b7d8d50744aed5483d359c88c90ee1297653aab6
abhishek-gajjar <Abhishek>
2022-10-10 20:48:02 -0400

    Changes in Code

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/HelloWorldController.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/HelloWorldController.java
@@ -7,9 +7,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloWorldController {
 
-	@GetMapping(path = "/hello-world")
-	public String helloWorld() {
-		return "Hello World";
+	@GetMapping(path = "/hello-world/{name}")
+	public String helloWorld(@PathVariable String name) {
+		return "Hello "+name;
 	}
 
 	@GetMapping(path = "/hello-world-bean")
